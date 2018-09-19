a7cc0a779a2c45ef8455adf978b57b3e6836ab51
epa15141 <36652580+epa15141@users.noreply.github.com>
2018-09-19 17:45:46 -0500

    Initial commit

---
+++ app/src/main/java/com/infordata/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.infordata.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://projects.sergiodinislopes.pt/flexdatalist/");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+}
